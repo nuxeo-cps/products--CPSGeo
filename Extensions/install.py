b4cd273ad6b4d2c7fca7b106d7a35595d2da6130
janguenot <devnull@localhost>
2005-09-05 13:19:56 +0000

    Auto install CPS Map Tool

--- Extensions/install.py
+++ Extensions/install.py
@@ -1,18 +1,23 @@
 
 from Products.CPSInstaller.CPSInstaller import CPSInstaller
 
-class MyInstaller(CPSInstaller):
+class CPSGeoInstaller(CPSInstaller):
         
     product_name = 'CPSGeo'
 
     def install(self):
         self.log("Starting CPSGeo install")
-        # Do the installing here
+        self.setupMapTool()
         self.finalize()
         self.log("End of specific CPSGeo install")
 
+    def setupMapTool(self):
+        self.log("Checking Map Tool")
+        self.verifyTool('portal_maps',
+                        'CPSGeo',
+                        'CPS Map Tool')
 def install(self):
-    installer = MyInstaller(self)
+    installer = CPSGeoInstaller(self)
     installer.install()
     return installer.logResult()
 

8cf19559675cc92f1b3b0ac6d3a61ae422ae4523
janguenot <devnull@localhost>
2005-09-14 02:06:00 +0000

    script to register the widgets

---
+++ skins/cpsgeo_document/getCPSGeoWidgets.py
@@ -0,0 +1,17 @@
+##parameters=
+#$Id$
+
+"""
+Here are defined the list of widgets to be registred
+Please, follow the same pattern to add new ones
+"""
+
+widgets = {
+    'Map Widget': {
+    'type': 'CPS Map Widget Type',
+    'data': {},
+    },
+
+    }
+return widgets
+

fa759a5377f8a77d0f8d15b5cedca21e44c21aad
janguenot <devnull@localhost>
2005-09-14 23:14:38 +0000

    remove thoses tests for now. They are wrong and need to be check

--- tests/testMbInstaller.py
+++ tests/testMbInstaller.py
@@ -7,7 +7,6 @@ import unittest
 
 from Testing import ZopeTestCase
 from Products.CPSGeo.Extensions.mapbuilder_installer import install_lib
-from Products.CPSGeo.Extensions.mapbuilder_installer import install_demo_app
 
 ZopeTestCase.installProduct('CPSGeo')
 
@@ -16,13 +15,7 @@ class MbInstallerTest(ZopeTestCase.ZopeTestCase):
     def test_install_lib(self):
         install_lib(self.folder)
         self.assert_(len(self.folder.mapbuilder.lib._objects) == 10,
-                     self.folder.mapbuilder.lib._objects)
-        
-    def test_install_demo(self):
-        install_demo_app(self.folder)
-        self.assert_(len(self.folder.demo._objects) == 6,
-                     self.folder.demo._objects)
-        
+                     self.folder.mapbuilder.lib._objects)        
 
 def test_suite():
     suite = unittest.TestSuite()

f22eaa8eb28edf3012059e534a580c75244fcef8
janguenot <devnull@localhost>
2005-09-19 23:20:15 +0000

    i18n / fix python script

--- skins/cpsgeo_document/cps_geolocate.py
+++ skins/cpsgeo_document/cps_geolocate.py
@@ -5,8 +5,14 @@ $Id$
 """
 
 if REQUEST is not None:
+
     form = REQUEST.form
-    pos_list = form['pos_list']
+
+    pos_list = ""
+    if form.has_key('pos_list'):
+        pos_list = form['pos_list']
+
+    # Update metadata
     context.getContent().edit(pos_list=pos_list)
 
     psm = 'psm_document_geolocated'

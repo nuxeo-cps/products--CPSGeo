b29213baa9335bcc8df2a3bad3c16a4d6df429fb
janguenot <devnull@localhost>
2005-09-19 10:09:59 +0000

    proper CPSGeoTestCase for integration

--- tests/CPSGeoTestCase.py
+++ tests/CPSGeoTestCase.py
@@ -1,11 +1,31 @@
 from Testing import ZopeTestCase
+from Products.ExternalMethod.ExternalMethod import ExternalMethod
 from Products.CPSDefault.tests import CPSTestCase
 
 ZopeTestCase.installProduct('CPSGeo')
 
-CPSTestCase.setupPortal()
-
 class CPSGeoTestCase(CPSTestCase.CPSTestCase):
     pass
+
+class CPSGeoInstaller(CPSTestCase.CPSInstaller):
+
+    def addPortal(self, id):
+
+        # Install the CPS Portal
+        CPSTestCase.CPSInstaller.addPortal(self, id)
+
+        # Install the CPSGeo product
+        portal = getattr(self.app, id)
+        if 'cps_geo_installer' not in portal.objectIds():
+            installer = ExternalMethod(
+                'cps_geo_installer',
+                '',
+                'CPSGeo.install',
+                'install')
+            portal._setObject('cps_geo_installer',
+                              installer)
+        portal.cps_geo_installer()
+
+CPSTestCase.setupPortal(PortalInstaller=CPSGeoInstaller)
 
 

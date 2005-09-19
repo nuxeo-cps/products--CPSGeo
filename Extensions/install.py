330a84a7955874b8098821d572e5b1fa7340a1a9
janguenot <devnull@localhost>
2005-09-19 15:06:47 +0000

    Global action to see the map server outside the content space

--- Extensions/install.py
+++ Extensions/install.py
@@ -1,4 +1,23 @@
+# -*- coding: ISO-8859-15 -*-
+# Copyright (c) 2005 Nuxeo SARL <http://nuxeo.com>
+# Author : Julien Anguenot <ja@nuxeo.com>
 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# $Id: CPSMapDocument.py 26799 2005-09-13 16:44:28Z janguenot $
+
+from Products.CMFCore.permissions import ManagePortal
 from Products.CPSInstaller.CPSInstaller import CPSInstaller
 
 from Products.CPSGeo.Extensions.mapbuilder_installer import install_lib
@@ -27,6 +46,7 @@ class CPSGeoInstaller(CPSInstaller):
         self.verifyVocabularies(self.portal.getCPSGeoVocabularies())
         self.setupFlexibleTypes()
         self.setupMapBuilderLibs()
+        self.setupCPSGeoActions()
         self.setupTranslations()
         self.finalize()
         self.log("End of specific CPSGeo install")
@@ -72,6 +92,29 @@ class CPSGeoInstaller(CPSInstaller):
         self.verifyLocalWorkflowChains(self.portal[WORKSPACES_ID], ws_chain)
         self.verifyLocalWorkflowChains(self.portal[SECTIONS_ID], se_chain)
 
+    def setupCPSGeoActions(self):
+
+        self.verifyActionProvider('portal_maps')
+
+        # Cleaning actions
+        actiondelmap = {
+            'portal_maps': ('cps_map_server',
+                                 )
+        }
+        self.deleteActions(actiondelmap)
+
+        # category : global
+        self.portal['portal_maps'].addAction(
+            id='cps_map_server',
+            name='action_cps_map_server',
+            action='string:${portal_url}/portal_maps/cps_map_server',
+            condition="",
+            permission=(ManagePortal,),
+            category='global',
+            visible=1)
+
+        self.log(" Added Action cps map server")
+        
 def install(self):
     installer = CPSGeoInstaller(self)
     installer.install()

314cd156551192b01cfd85b80a55c928ec3ed196
janguenot <devnull@localhost>
2005-09-20 01:05:07 +0000

    Add the possiblity to record a search request. The results will appear on the map

--- skins/cpsgeo_document/getCPSGeoLayouts.py
+++ skins/cpsgeo_document/getCPSGeoLayouts.py
@@ -86,7 +86,113 @@ map_document_layout = {
             },
         },
 
-        
+        'results': {
+           'type': 'InternalLinks Widget',
+            'data': {
+                'title': '',
+                'fields': ('results',),
+                'is_required': False,
+                'label_edit': 'label_cpsgeo_search_results',
+                'is_i18n': True,
+                'hidden_empty': False,
+                'new_window': False,
+                'size': 0,
+                'absolute': True,
+            },
+        },
+        'query_fulltext': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_fulltext'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_full_text',
+                'display_width': 40,
+                'size_max': 100,
+            },
+        },
+        'query_title': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_title'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_title',
+                'display_width': 40,
+                'size_max': 100,
+            },
+        },
+        'query_description': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('query_description'),
+                'is_i18n': 1,
+                'label_edit': 'label_search_description',
+                'display_width': 40,
+                'size_max': 100,
+            },
+        },
+        'folder': {
+            'type': 'String Widget',
+            'data': {
+                'fields': ('folder'),
+                'is_i18n': 1,
+                'label_edit': 'label_folder',
+                'display_width': 40,
+                'size_max': 100,
+            },
+        },
+        'nb_items': {
+            'type': 'Int Widget',
+            'data': {
+                'fields': ('nb_items'),
+                'is_i18n': 1,
+                'label_edit': 'label_nb_items',
+                'display_width': 3,
+            },
+        },
+        'sort_by': {
+            'type': 'Select Widget',
+            'data': {
+                'fields': ['sort_by'],
+                'is_i18n': 1,
+                'label_edit': 'label_sort_by',
+                'vocabulary': 'search_sort_results_by',
+                'translated': 1,
+                },
+            },
+        'query_status': {
+            'type': 'Select Widget',
+            'data': {
+                'fields': ['query_status'],
+                'is_i18n': 1,
+                'label_edit': 'label_search_status',
+                'vocabulary': 'navigation_filter_review_state',
+                'translated': 1,
+                },
+            },
+        'query_portal_type': {
+            'type': 'MultiSelect Widget',
+            'data': {
+                'fields': ['query_portal_type'],
+                'is_i18n': 1,
+                'label_edit': 'label_search_portal_type',
+                'vocabulary': 'navigation_filter_listing_ptypes',
+                'size': 10,
+                },
+            },
+        'search': {
+            'type': 'Search Widget',
+            'data': {
+                'fields': (),
+                'label_edit': 'label_search',
+                'help': 'help_newsletter_search_widget',
+                'is_i18n': 1,
+                'css_class': 'articleELink',
+                'widget_ids': ['query_fulltext',
+                               'query_title', 'query_description',
+                               'query_status', 'query_portal_type',
+                               'folder', 'nb_items', 'sort_by'],
+                },
+            }, 
     },
     'layout': {
         'style_prefix': 'layout_default_',
@@ -99,7 +205,11 @@ map_document_layout = {
         'rows': [
             [{'widget_id': 'map_id', 'ncols': 1},
             ],
-            [{'widget_id': 'map_rendering', 'ncols': 1},
+            [{'widget_id': 'map_rendering', 'ncols': 1}, 
+            ],
+            [{'widget_id': 'results', 'ncols': 1},
+            ],
+            [{'widget_id': 'search', 'ncols': 1},
             ],
         ],
     },

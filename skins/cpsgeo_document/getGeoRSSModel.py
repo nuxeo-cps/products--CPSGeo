80f10d49a3da77e4c1cbf615971da1171d89aa7a
sgillies <devnull@localhost>
2005-09-20 04:07:32 +0000

    brains fetched

--- skins/cpsgeo_document/getGeoRSSModel.py
+++ skins/cpsgeo_document/getGeoRSSModel.py
@@ -11,11 +11,10 @@
 from Products.CPSGeo.georss import brainsToGeoRSS
 
 results = context.getContent().results
-return results
-
-#brainsToGeoRSS(context.title, context.absolute_url(), context.getContent().results)
-#container.REQUEST.RESPONSE.setHeader('Content-type', 'text/xml')
-#return context.portal_maps.brainsToGeoRSS(context.title, context.absolute_url(), context.getContent().results)
-
-
+brains = []
+for r in results:
+    brain = context.portal_catalog(path=r)
+    brains.append(brain)
+container.REQUEST.RESPONSE.setHeader('Content-type', 'text/xml')
+return brainsToGeoRSS(context.title, context.absolute_url(), brains)
 

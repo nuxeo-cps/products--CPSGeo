56c1ede741ef249fda09a9b70cf88e597ade617c
janguenot <devnull@localhost>
2005-11-07 01:54:16 +0000

    Comments and cleanup

--- skins/cpsgeo_widgets/getCurrentCoordinates.py
+++ skins/cpsgeo_widgets/getCurrentCoordinates.py
@@ -1,16 +1,13 @@
-## Script (Python) "getCurrentCoordinates"
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
 ##parameters=
-##title=
-##
+""" Return the coordinate of the context.
+
+If it's not set then return 0.0,0.0 as default value.
+
+$Id: $
+"""
 
 pos_list = getattr(context.getContent(), 'pos_list', False)
 if pos_list:
     return pos_list.replace(' ',',')
-else:
-    return '0.0,0.0'
+return '0.0,0.0'
 

d94fc196ad8a8d5493d4f10f69418828596c9639
janguenot <devnull@localhost>
2005-11-09 22:42:37 +0000

    Comments and INFO logs

--- __init__.py
+++ __init__.py
@@ -15,31 +15,56 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 # $Id: CPSPortlet.py 26680 2005-09-09 14:22:18Z janguenot $
+"""CPSGeo : GIS support for CPS3
 
-# We suppport both for now. lxml has a lot of problems running on old
-# platforms
+http://www.cps-project.org/sections/projects/cpsgeo_simple_gis_for
+"""
+
+from zLOG import LOG, INFO
+logKey = 'CPSGeo'
+
+#
+# Check first if elementtree is installed. If not it will use lxml.
+# lxml runs well but on latest plateform
+# See the DEPENDENCIES.txt file for more info on the revisions
+#
+
 try:
     import elementtree.ElementTree as etree
     # Monkey Patch adds to the default well known namespaces
     etree._namespace_map.update({
-        "http://www.w3.org/1999/02/22-rdf-syntax-ns#":  "rdf", 
-        "http://purl.org/rss/1.0/":                     "rss", 
-        "http://purl.org/rss/1.0/modules/taxonomy/":    "taxo", 
-        "http://purl.org/dc/elements/1.1/":             "dc", 
-        "http://purl.org/rss/1.0/modules/syndication/": "syn", 
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#":  "rdf",
+        "http://purl.org/rss/1.0/":                     "rss",
+        "http://purl.org/rss/1.0/modules/taxonomy/":    "taxo",
+        "http://purl.org/dc/elements/1.1/":             "dc",
+        "http://purl.org/rss/1.0/modules/syndication/": "syn",
         "http://www.w3.org/2003/01/geo/wgs84_pos#":     "geo"})
-except ImportError:
-    import lxml.etree as etree
+    LOG(logKey, INFO, "will use elementtree as XML library")
+except ImportError, e:
+    if 'No module named elementtree' not in str(e):
+        raise
+    try:
+        import lxml.etree as etree
+        LOG(logKey, INFO, "will use lxml as XML library")
+    except ImportError, e:
+        if 'No module named lxml' not in str(e):
+            msg_ = "requires either elementtree or lxml"
+            LOG(logKey, INFO, msg_)
+            raise logKey + msg_
+
+## EOF XML lib checks
 
 from Products.CMFCore import utils
 from Products.CMFCore.permissions import AddPortalContent
 from Products.CMFCore.DirectoryView import registerDirectory
 from Products.CMFCore.DirectoryView import registerFileExtension
+from Products.CMFCore.FSFile import FSFile
 
 # Register the .xml and .xslt as possible extension for the FSFile
-from Products.CMFCore.FSFile import FSFile
 registerFileExtension('xml', FSFile)
+LOG(logKey, INFO, "Registers .xml extension for CMFCore.FSFile")
 registerFileExtension('xsl', FSFile)
+LOG(logKey, INFO, "Registers .xslt extension for CMFCore.FSFile")
 
 import MapTool
 import Map
